2815ca5e4ef80e0ababe013cf23ea7c99d3e4def
bestpie <bestpie2004@gmail.com>
2022-04-16 16:01:32 -0700

    Updated Status & Fixed Errors

--- index.js
+++ index.js
@@ -5,28 +5,44 @@ config({ path: `${__dirname}/.env` })
 
 rpc.on('ready', () => {
     console.clear()
+    if (!process.env.ClientID) { return console.log(`Please Enter a ClientID. Without the ClientID then this Rich Presence won't work.`) }
+
+    //State
+    if (process.env.State) { if (process.env.State.length = 1) { state = process.env.State + ' ' } else { state = process.env.State } } else { state = '  ' }
+    //Details
+    if (process.env.Details) { if (process.env.Details.length = 1) { details = process.env.Details + ' ' } else { details = process.env.Details } } else { details = '  ' }
+    //Start/End/Enable Timestamps
+    if (process.env.StartTimeEnabled === 'true') { starttimestamp = new Date(); endtimestamp = undefined } else if (process.env.StartTimeEnabled === 'false') { if (process.env.StartTimestamp === '') { starttimestamp = undefined } else { starttimestamp = process.env.StartTimestamp } if (process.env.EndTimestamp === '') { endtimestamp = undefined } else { endtimestamp = process.env.EndTimestamp } }
+    //Large Image
+    if (process.env.LargeImage) { largeimage = process.env.LargeImage; if (process.env.LargeImageTooltip.length == 1) { largeimagetooltip = process.env.LargeImageTooltip + ' ' } else { if (process.env.LargeImageTooltip === '') { largeimagetooltip = '  ' } else largeimagetooltip = process.env.LargeImageTooltip } }
+    //Small Image
+    if (process.env.SmallImage) { smallimage = process.env.SmallImage; if (process.env.SmallImage.length == 1) { smallimagetooltip = process.env.SmallImageTooltip + ' ' } else { if (process.env.SmallImageTooltip === '') { smallimagetooltip = '  ' } else smallimagetooltip = process.env.SmallImageTooltip } }
+    //Buttons
+    //if (process.env.Button1) { if (button1 = '') { button1label = undefined; button1url = undefined } else button1label = process.env.Button1 }
+
     rpc.setActivity({
-        details: process.env.Details,
-        state: process.env.State,
-        startTimestamp: new Date(),
-        largeImageKey: process.env.LargeImage,
-        largeImageText: process.env.LargeImageTooltip,
-        smallImageKey: process.env.SmallImage,
-        smallImageText: process.env.SmallImageTooltip,
+        details: details,
+        state: state,
+        startTimestamp: starttimestamp,
+        endTimestamp: endtimestamp,
+        largeImageKey: largeimage,
+        largeImageText: largeimagetooltip,
+        smallImageKey: smallimage,
+        smallImageText: smallimagetooltip,
         // buttons: [{
-        //     label: process.env.Button1,
-        //     url: process.env.Button1url
+        //     label: undefined,
+        //     url: undefined
         // }, {
-        //     label: process.env.Button2,
-        //     url: process.env.Button2url
+        //     label: undefined,
+        //     url: undefined
         // }]
     })
     console.log(`Current Presence (${process.env.ClientID})
-State: ${process.env.State}
-Details: ${process.env.Details}
-Large Image: ${process.env.LargeImage}
-Small Image: ${process.env.SmallImage}
-Displaying Presence = True`)
+State: ${state}
+Details: ${details}
+Large Image: ${largeimage}
+Small Image: ${smallimage}
+Displaying Rich Presence For ${rpc.user.username}#${rpc.user.discriminator}`)
 })
 
 rpc.login({ clientId: process.env.ClientID })
